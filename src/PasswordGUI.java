5b3a12bf94e3ee0090ee67a3d19909bf916c7416
drentawc <wdrenta123@gmail.com>
2022-11-02 15:53:18 +0100

    Further work on GUI class

--- src/PasswordGUI.java
+++ src/PasswordGUI.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
@@ -15,15 +16,18 @@ public class PasswordGUI implements ActionListener {
     FileIO io;
     JFrame mainFrame;
     JFrame passFrame;
+    JFrame masterFrame;
     JTextField accountField;
     JTextField userField;
     JTextField passField;
+    JTextField masterField;
     JTable table;
     JPanel panel;
     //JList<String> usernameList;
     //JList<String> passwordList;
     String passwordFile;
     String keyFile;
+    Boolean decryptedFlag;
     HashMap<String, Entry<String, String>> encryptedPasswords;
     HashMap<String, Entry<String, String>> decryptedPasswords;
 
@@ -68,19 +72,106 @@ public class PasswordGUI implements ActionListener {
         addPassword.addActionListener(this);
         addPassword.setActionCommand("Add");
 
-        this.mainFrame.setSize(700, 600);
+        JButton decrypt = new JButton("Decrypt/Encrypt");
+
+        c.gridx = 0;
+        c.gridy = 2;
+        this.mainFrame.add(decrypt, c);
+
+        decrypt.addActionListener(this);
+        decrypt.setActionCommand("Decrypt/Encrypt");
+
+        this.mainFrame.setSize(800, 700);
+    }
+
+    public void checkPassword() {
+        this.masterFrame = new JFrame("Enter Master Password");
+
+        this.masterFrame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = SwingConstants.VERTICAL;
+
+        c.gridx = 0;
+        c.gridy = 1;
+        JLabel passLabel = new JLabel("Password");
+        this.masterFrame.add(passLabel, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        this.masterField = new JTextField();
+        this.masterFrame.add(this.masterField, c);
+
+        JButton enter = new JButton("Enter");
+        c.gridx = 1;
+        c.gridy = 2;
+        this.masterFrame.add(enter, c);
+
+        enter.addActionListener(this);
+        enter.setActionCommand("master");
+
+        this.masterFrame.setSize(300, 300);
+        this.masterFrame.setVisible(true);
+        this.masterFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public void display() {
-        this.mainFrame.setVisible(true);
-        this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        checkPassword();
+    }
+
+    public void updateJTable() {
+
+        if (this.decryptedFlag) {
+            this.encryptedPasswords = io.readPasswords(this.passwordFile);
+
+            if (!this.encryptedPasswords.isEmpty()) {
+    
+                String[] columns = { "Account", "Username", "Password"};
+                String[][] data = getTableData(this.encryptedPasswords);
+    
+                System.out.println("Create");
+                for (int x = 0; x < data.length; x++) {
+                    System.out.println(data[x][1]);
+                }
+                System.out.println();
+
+                this.decryptedFlag = false;
+
+                DefaultTableModel model = new DefaultTableModel(data, columns);
+     
+                this.table.setModel(model);
+            }
+        } else {
+            System.out.println("Decrypt");
+            decryptAllPasswords();
+
+            if (!this.decryptedPasswords.isEmpty()) {
+                String[] columns = { "Account", "Username", "Password"};
+                String[][] data = getTableData(this.decryptedPasswords);
+
+                for (int x = 0; x < data.length; x++) {
+                    System.out.println(data[x][0]);
+                    System.out.print(data[x][1]);
+                    System.out.print(data[x][2]);
+                }
+                System.out.println();
+
+                this.decryptedFlag = true;
+
+                DefaultTableModel model = new DefaultTableModel(data, columns);
+
+                this.table.setModel(model);
+            }
+
+        }
     }
 
     public void createJTable() {
+
         this.encryptedPasswords = io.readPasswords(this.passwordFile);
 
+        this.decryptedFlag = false;
+
         if (!this.encryptedPasswords.isEmpty()) {
-            int size = encryptedPasswords.size();
 
             String[] columns = { "Account", "Username", "Password"};
             String[][] data = getTableData(this.encryptedPasswords);
@@ -90,8 +181,10 @@ public class PasswordGUI implements ActionListener {
                 System.out.println(data[x][1]);
             }
             System.out.println();
+
+            DefaultTableModel model = new DefaultTableModel(data, columns);
  
-            this.table = new JTable(data, columns);
+            this.table = new JTable(model);
         }
     }
 
@@ -115,6 +208,34 @@ public class PasswordGUI implements ActionListener {
             addPasswords();
             this.passFrame.setVisible(false);
             this.passFrame.dispose();
+        } else if (e.getActionCommand().equals("Decrypt/Encrypt")) {
+            decryptAllPasswords();
+
+            updateJTable();
+        } else if (e.getActionCommand().equals("master")) {
+            this.masterFrame.setVisible(false);
+            this.masterFrame.dispose();
+
+            System.out.println(this.masterField.getText());
+
+            /**
+             * 
+             * INSTEAD ADD THIS FUNCTIONALITY TO DECRYPT NOT TO START OF PROGRAM OR BOTH
+             * 
+             */
+
+
+
+            if (this.masterField.getText().equals("asd")) {
+
+                this.mainFrame.setVisible(true);
+                this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            } else {
+                System.out.println("Wrong master password, exiting....");
+                this.masterFrame.setVisible(false);
+                this.masterFrame.dispose();
+            }
         }
     }
 
@@ -124,15 +245,12 @@ public class PasswordGUI implements ActionListener {
             String tempAccount = e.encrypt(this.accountField.getText());
             Entry<String, String> tempEntry = new AbstractMap.SimpleEntry<String, String>(e.encrypt(this.userField.getText()), e.encrypt(this.passField.getText()));
 
+            this.decryptedFlag = true;
+
             this.encryptedPasswords.put(tempAccount, tempEntry);
             io.writePassword(this.passwordFile, tempAccount, tempEntry);
 
-            createJTable();
-            this.table.revalidate();
-            this.table.repaint();
-            this.panel.revalidate();
-            this.panel.repaint();
-            //formatGUI();
+            updateJTable();
         }
     }
 
@@ -183,6 +301,44 @@ public class PasswordGUI implements ActionListener {
 
         this.passFrame.setSize(200, 200);
         this.passFrame.setVisible(true);
+    }
+
+    public void decryptAllPasswords() {
+
+        for (Entry<String,Entry<String,String>> entry : this.encryptedPasswords.entrySet()) {
+            String account = this.e.decrypt(entry.getKey());
+
+            Entry<String, String> encryptedEntry = entry.getValue();
+
+            System.out.println(account);
+
+            Entry<String, String> decryptedEntry = new AbstractMap.SimpleEntry<String, String>(e.decrypt(encryptedEntry.getKey()), e.decrypt(encryptedEntry.getValue()));
+
+            this.decryptedPasswords.put(account, decryptedEntry);
+        }
+
+        // String[][] data = getTableData(this.encryptedPasswords);
+
+        // for (int x = 0; x < data.length; x++) {
+        //     System.out.println(data[x][1]);
+        // }
+
+        // if (!this.encryptedPasswords.isEmpty()) {
+        //     int size = encryptedPasswords.size();
+
+        //     String[] columns = { "Account", "Username", "Password"};
+        //     String[][] data = getTableData(this.encryptedPasswords);
+
+        //     // System.out.println("Create");
+        //     // for (int x = 0; x < data.length; x++) {
+        //     //     System.out.println(data[x][1]);
+        //     // }
+        //     // System.out.println();
+ 
+        //     this.table = new JTable(data, columns);
+        // }
+
+
     }
 
 }
